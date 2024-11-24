c9370114692d528b92878a8e68ac12926c4085fe
Magnus Ihse Bursie <mag@icus.se>
2024-11-24 13:02:34 +0100

    Update hacs.json and fix comment

--- custom_components/bleep/__init__.py
+++ custom_components/bleep/__init__.py
@@ -2,6 +2,6 @@ from homeassistant import core
 
 
 async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
-    """Set up the HA bleep (BLE ePaper) component."""
+    """Set up the BLE ePaper (bleep) component."""
     # @TODO: Add setup code.
     return True
